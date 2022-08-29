1dc6a1c1339c9469ac072421993fbd09503d44bb
Vivek kumar <57774129+theVivek-kumar@users.noreply.github.com>
2022-08-29 16:19:39 +0530

    form

---
+++ assignment1/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import './Components/Navbar/Navbar.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+    </div>
+  );
+}
+
+export default App;
